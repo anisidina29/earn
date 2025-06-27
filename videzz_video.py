599b0ff73dd59974b130d02c2a83757d8a2252dc
Chip Huyen <thanh6301@gmail.com>
2025-06-27 12:39:55 +0700

    Update videzz_video.py

--- videzz_video.py
+++ videzz_video.py
@@ -2,7 +2,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import undetected_chromedriver as uc
 from selenium.webdriver.common.action_chains import ActionChains
 import random
 from fake_useragent import UserAgent
